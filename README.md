3789f2e5dc4a31de54364b2b42af36df082004eb
bangshinchul <bsc0227@naver.com>
2020-07-01 00:33:37 +0900

    second commit

--- README.md
+++ README.md
@@ -21,3 +21,18 @@ samples, guidance on mobile development, and a full API reference.
 - https://flutter-ko.dev/docs/get-started/codelab
 - https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#0
 - https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#0
+
+
+다음 리소스에서 Flutter SDK에 대해 자세히 알아보십시오.
+- [Flutter의 레이아웃](https://flutter.dev/docs/development/ui/layout)
+- [Flutter 앱에 대화 형 기능 추가](https://flutter.dev/docs/development/ui/interactive)
+- [위젯 소개](https://flutter.dev/docs/development/ui/widgets-intro)
+- [안드로이드 개발자를위한 Flutter](https://flutter.dev/docs/get-started/flutter-for/android-devs)
+- [React Native 개발자를위한 Flutter](https://flutter.dev/docs/get-started/flutter-for/react-native-devs)
+- [웹 개발자를위한 Flutter](https://flutter.dev/docs/get-started/flutter-for/react-native-devs)
+- [Flutter의 YouTube 채널](https://flutter.dev/docs/get-started/flutter-for/react-native-devs)
+
+
+다른 자료들 :
+- [자세한 해설서](https://flutter.dev/docs/get-started/flutter-for/react-native-devs)
+- [Dart.js에 부트 스트랩](https://flutter.dev/docs/get-started/flutter-for/react-native-devs)
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
     );
   }
@@ -166,7 +167,7 @@ class _RandomWordsState extends State<RandomWords> {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: /*1*/ (context, i) { // i는 index입니다. itemBuilder 콜백은 단어 쌍이 제안될 때마다 호출되고 각각을 ListTile 행에 배치합니다. index가 짝수 행인 경우 ListTile 행에 단어 쌍을 추가합니다. 홀수 행인 경우 시각적으로 각 항목을 구분하는 Divider 위젯을 추가합니다. 작은 기기에서는 구분선을 보기 어려울 수 있습니다.
-        if (i.isOdd) return Divider(color: Colors.blue); /*2*/ // ListView의 각 행 앞에 1 픽셀 높이의 구분선 위젯을 추가하십시오. isOdd는 정수가 홀수인 경우에만 true를 리턴합니다.
+        if (i.isOdd) return Divider(); /*2*/ // ListView의 각 행 앞에 1 픽셀 높이의 구분선 위젯을 추가하십시오. isOdd는 정수가 홀수인 경우에만 true를 리턴합니다.
 
         final index = i ~/ 2; /*3*/ // i ~/ 2 표현식은 i를 2로 나눈 뒤 정수 결과를 반환합니다. 예를 들어: 1, 2, 3, 4, 5는 0, 1, 1, 2, 2가 됩니다. 이렇게 하면 구분선 위젯을 제외한 ListView에 있는 단어 쌍 수가 계산됩니다.
         if (index >= _suggestions.length) {
